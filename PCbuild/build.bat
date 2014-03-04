0ecd6b655027bcb36dfe8b7ac20a43faa38ec8aa
Wang Xuerui <idontknw.wang@gmail.com>
2014-03-04 20:29:11 +0800

    change default configuration to x64

--- PCbuild/build.bat
+++ PCbuild/build.bat
@@ -3,7 +3,7 @@ rem A batch program to build or rebuild a particular configuration.
 rem just for convenience.
 
 setlocal
-set platf=Win32
+set platf=x64
 set conf=Release
 set build=
 
@@ -13,6 +13,6 @@ if "%1"=="-p" (set platf=%2) & shift & shift & goto CheckOpts
 if "%1"=="-r" (set build=/rebuild) & shift & goto CheckOpts
 if "%1"=="-d" (set conf=Debug) & shift & goto CheckOpts
 
-set cmd=vcbuild /useenv pcbuild.sln %build% "%conf%|%platf%"
+set cmd=msbuild /useenv pcbuild.sln %build% "%conf%|%platf%"
 echo %cmd%
 %cmd%
